279733c5351796839d01d548e59af22d9b8c23c9
SarperArda <91608321+SarperArda@users.noreply.github.com>
2024-04-16 18:31:41 +0300

    Clone sample project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    
+    name="sarper-arda-bakir-dictionary",
+    author="Sarper Arda BAKIR",  
+    version="2.0.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
